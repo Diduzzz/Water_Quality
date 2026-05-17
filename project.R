c2f76f57b4578085fe37611342dffe9243b60757
Diduzzz <rhendell.matalog24@gmail.com>
2026-05-17 11:05:07 +0200

    update

--- project.R
+++ project.R
@@ -27,7 +27,71 @@ round(prop.table(table(train_set$Potability)),3)
 cat("\n--- Proporzioni nel Test Set ---\n")
 round(prop.table(table(test_set$Potability)),3)
 
-## Counting misisng values
+## IMPUTAZIONE CON MISSFOREST 
+# prima cosa visualizziamo i missing values di tutte le colonne
 missing_values = colSums(is.na(data))/nrow(data) * 100
 print(missing_values)
+# rimuoviamo temporaneamente il target per l'imputazione
+set.seed(123)
+train_imputed_obj <- missForest(as.data.frame(train_set[, -which(names(train_set) == "Potability")]))
+train_final <- data.frame(train_imputed_obj$ximp)
+train_final$Potability <- train_set$Potability
+
+# facciamo la stessa cosa per per il Test set
+test_imputed_obj <- missForest(as.data.frame(test_set[, -which(names(test_set) == "Potability")]))
+test_final <- data.frame(test_imputed_obj$ximp)
+test_final$Potability <- test_set$Potability
+
+nzv <- nearZeroVar(train_final, saveMetrics = TRUE)
+print(nzv)
+
+#check missing values after imputation
+missing_values_after_imputation = colSums(is.na(train_final))/nrow(train_final) * 100
+print(missing_values_after_imputation)
+
+## ANALISI DELLA MULTICOLLINEARITA
+cor_matrix <- cor(train_final[, sapply(train_final, is.numeric)])
+corrplot(cor_matrix, method = "color", type = "upper", addCoef.col = "black", number.cex = 0.7)
+
+# Se trovi correlazioni > 0.90:
+# high_cor <- findCorrelation(cor_matrix, cutoff = 0.9)
+# train_final <- train_final[, -high_cor]
+
+## BILANCIAMENTO DELLE CLASSI
+cat("\n--- Proporzioni nel Training Set ---\n")
+round(prop.table(table(train_final$Potability)),3)
+
+set.seed(123)
+train_balanced <- ROSE(Potability~., data = train_final, seed = 123)$data
+print(round(prop.table(table(train_balanced$Potability)),3))
+
+## MODEL SELECTION CON RANDOM FOREST E CROSS VALIDATION
+set.seed(1)
+
+cvCtrl <- trainControl(
+  method = "cv",
+  number = 5,
+  classProbs = TRUE,
+  summaryFunction = twoClassSummary
+)
+fit_rf <- train(Potability ~ ., 
+                data = train_balanced, 
+                method = "rf", 
+                metric = "ROC",
+                trControl = cvCtrl)
+
+vimp <- varImp(fit_rf, scale = TRUE)
+print(vimp)
+plot(vimp, main = "Importanza delle Variabili - Random Forest")
+
+vimp=data.frame(vimp[1])
+vimp$var=row.names(vimp)
+vimp2=vimp[vimp$Overall>0,]
+
+selected_features <- row.names(vimp2)
+# subset di TRAINING con le sole variabili selezionate + il TARGET
+df_subset <- train_balanced[, c(selected_features, "Potability")]
+# subset di TEST con le STESSE identiche variabili + il TARGET
+dataTestSubset <- test_final[, c(selected_features, "Potability")]
+
 
